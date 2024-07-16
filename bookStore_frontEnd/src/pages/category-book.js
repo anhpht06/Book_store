b5d01604b4bc96a6d45bebe2039814b107c7b340
tuanAnh <tuanichi06@gmail.com>
2024-07-16 15:43:43 +0700

    delete profile frontEnd

--- bookStore_frontEnd/src/pages/category-book.js
+++
@@ -1,7 +0,0 @@
-export default function CategoryBook() {
-  return (
-    <div>
-      <h1>Danh sách sách</h1>
-    </div>
-  );
-}
--- bookStore_frontEnd/src/pages/login.js
+++
@@ -1,8 +0,0 @@
-import ILogin from "@/component/login"
-function Login(){
-    return (
-        <ILogin/>
-    )
-}
-
-export default Login
